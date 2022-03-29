da9b65ab97ea98f09f954f640bd540f971111274
Krzysztof Lach <lachkrzysztof56@gmail.com>
2022-03-29 16:24:05 +0200

    Learn print statment and execution flow

---
+++ 1_basic_info.dart
@@ -0,0 +1,21 @@
+
+// Lesson 1 Basic Infromation
+
+// Print Hello world
+void main() {
+  print("Hello World!"); // Semicolon very important
+
+  // Drow little triangle
+  // Creating function
+  void triangle() {
+    print("   /|");
+    print("  / |");
+    print(" /  |");
+    print("/___|");
+
+  }
+
+  // Run function
+  triangle();
+}
+
